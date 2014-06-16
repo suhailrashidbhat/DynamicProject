df1f44ab3e86d354981c7e90a88e0db9776a01a5
Suhail Rashid Bhat <suhail.bhat@crichq.com>
2014-06-16 10:48:59 +0530

    InitialComit

--- DynamicsProject/ViewController.m
+++ DynamicsProject/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
             
+@property (strong, nonatomic) IBOutlet UIImageView *movingImage;
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundimage;
 
 @end
 
@@ -18,11 +20,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.movingImage.image = [UIImage imageNamed:@"sorry1.gif"];
+    [self moveItBaby];
+    self.backgroundimage.image = [UIImage imageNamed:@"IMAG1092.jpg"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)moveItBaby {
+    UIInterpolatingMotionEffect *xAxis = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+    xAxis.minimumRelativeValue = @-80;
+    xAxis.maximumRelativeValue = @80;
+    
+    UIInterpolatingMotionEffect *yAxis = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    yAxis.minimumRelativeValue = @-80;
+    yAxis.maximumRelativeValue = @80;
+    
+    UIMotionEffectGroup *motionGroup = [[UIMotionEffectGroup alloc] init];
+    motionGroup.motionEffects = @[xAxis, yAxis];
+    
+//    UIDynamicAnimator *animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.movingImage];
+//    
+//    
+//    UIGravityBehavior *gravity = [[UIGravityBehavior alloc] initWithItems:@[self.backgroundimage]];
+//    gravity.gravityDirection = CGVectorMake(0.0, 4.0);
+//    gravity.action = ^{
+//    
+//    };
+//    [animator addBehavior:gravity];
+    
+    [self.movingImage addMotionEffect:motionGroup];
 }
 
 @end
